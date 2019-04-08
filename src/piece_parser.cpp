4fc1364ae789a6cda29bda777891c2f60c0e82e8
Mardava Gubbi <mgubbi1@jhu.edu>
2019-04-08 10:17:48 -0400

    Add Harris corner detection

--- src/piece_parser.cpp
+++ src/piece_parser.cpp
@@ -13,8 +13,12 @@
 #include <algorithm>
 #include <functional>
 
+#include <exception>
+
 using namespace cv;
 using namespace std;
+
+vector<Scalar> colours;
 
 class PieceParser
 {
@@ -36,6 +40,7 @@ class PieceParser
     void imageSubscriberCallback(const sensor_msgs::ImageConstPtr& msg);
     void binarizeImage(Mat img_input, int median_blur_size, int bin_threshold, int blur_kernel_size);
     vector< vector< Point> > findPieces(int area_threshold, int num_pixel_threshold, int edge_distance_threshold);
+    vector<Point> getEdges(vector<Point> piece_contour, int block_size, int aperture_size, double harris_free_param);
 };
 
 PieceParser::PieceParser(ros::NodeHandle& nh)
@@ -46,19 +51,110 @@ PieceParser::PieceParser(ros::NodeHandle& nh)
   this->image_sub = this->img_transport->subscribe("/camera/image", 1, &PieceParser::imageSubscriberCallback, this);
 }
 
+vector<Point> PieceParser::getEdges(vector<Point> piece_contour, int block_size, int aperture_size, double harris_free_param)
+{
+  int i;
+  int j;
+  int k;
+  int max_corner_points = 8; // Twice of what we need, should contain the required corners.
+
+  ROS_INFO("Initializing Harris corner matrix...");
+  Mat harris_corners = Mat::zeros(this->img_bin.size(), CV_32FC1);
+  ROS_INFO("Fetching Harris corners...");
+  ROS_INFO_STREAM("img_bin.type() = " << this->img_bin.type());
+  cornerHarris(this->img_bin, harris_corners, block_size, aperture_size, harris_free_param);
+  Point corner_points[max_corner_points];
+  double corner_vals[max_corner_points];
+  int num_indices = 0;
+
+  ROS_INFO("Checking Harris corners for top values...");
+
+  for(i = 0; i < piece_contour.size(); ++i)
+  {
+    // ROS_INFO_STREAM("(" << i << ", " << j << ")\t\t(" << corner_points[0].x << ", " << corner_points[0].y << ")");
+    const double harris_val_curr = harris_corners.at<double>(piece_contour[i].x, piece_contour[i].y);
+    // ROS_INFO_STREAM("Double assignment completed.");
+
+    for(j = 0; j < max_corner_points; ++j)
+    {
+      if(harris_val_curr > corner_vals[j])
+      {
+        ROS_INFO_STREAM(++num_indices << " peak points found.");
+
+        for(k = max_corner_points - 1; k > j; --k)
+        {
+          corner_vals[k] = corner_vals[k - 1];
+          corner_points[k] = corner_points[k - 1];
+        }
+
+        Point temp_pt = Point(piece_contour[i].x, piece_contour[i].y);
+        corner_vals[j] = harris_val_curr;
+        // corner_points[j] = temp_pt;
+        corner_points[0] = temp_pt;
+        break;
+      }
+      else
+      {
+        // No operation
+      }
+    }
+  }
+
+  ROS_INFO_STREAM("Converting to vector form...");
+
+  vector<Point> corner_points_vec;
+
+  for(i = 0; i < max_corner_points; ++i)
+  {
+    corner_points_vec.push_back(corner_points[i]);
+  }
+
+  ROS_INFO_STREAM("Maximum Harris corner values successfully obtained.");
+
+  return corner_points_vec;
+}
+
 void PieceParser::imageSubscriberCallback(const sensor_msgs::ImageConstPtr& msg)
 {
+  int i;
+  int j;
+
   // Binarize the image with preset thresholds.
   // TODO: tweak the thresholds if need be.
   ROS_INFO("Binarizing image...");
-  this->binarizeImage(cv_bridge::toCvShare(msg, "bgr8")->image, 5, 120, 3);
+  Mat img_raw =cv_bridge::toCvShare(msg, "bgr8")->image;
+  this->binarizeImage(img_raw, 5, 105, 3);
 
   // Obtain the connected components and their centroids.
   ROS_INFO("Finding puzzle pieces in image...");
   vector< vector<Point> > piece_contours = this->findPieces(100000, 100, 20);
+  ROS_INFO("Finding centroids of pieces in image...");
+  vector<Point2f> centroids(piece_contours.size());
+
+  for(i = 0; i < piece_contours.size(); ++i)
+  {
+    Moments mu = moments(piece_contours[i], false);
+    centroids[i] = Point2f(mu.m10 / mu.m00, mu.m01 / mu.m00);
+    drawContours(img_raw, piece_contours, i, colours[0], 2); // Thickness = 2
+    circle(img_raw, centroids[i], 16, colours[0], -1, 8, 0);
+  }
+
+  // TODO: tweak edge classification parameters if need be.
+  ROS_INFO("Finding Harris corners...");
+
+  for(i = 0; i < piece_contours.size(); ++i)
+  {
+    vector<Point> harris_corners = getEdges(piece_contours[i], 5, 5, 0.04);
+    ROS_INFO("Displaying Harris corners...");
+
+    for(j = 0; j < harris_corners.size(); ++j)
+    {
+      circle(img_raw, harris_corners[j], 16, colours[1], -1, 8, 0);
+    }
+  }
 
   namedWindow("display_window", WINDOW_AUTOSIZE);
-  imshow("display_window", img_bin);
+  imshow("display_window", img_raw);
   waitKey(1);
 }
 
@@ -67,10 +163,8 @@ void PieceParser::binarizeImage(Mat img_input, int median_blur_size, int bin_thr
   Mat img_blur;
   Mat img_thresh;
 
-  ROS_INFO("Checking if image sizes have already been defined...");
   if(this->img_bin.rows == 0)
   {
-    ROS_INFO("Initializing img_bin...");
     this->img_bin = Mat(img_input.size(), img_input.type());
   }
   else
@@ -78,18 +172,11 @@ void PieceParser::binarizeImage(Mat img_input, int median_blur_size, int bin_thr
     // No operation
   }
 
-  ROS_INFO("Converting to grayscale...");
   cvtColor(img_input, this->img_bin, CV_RGB2GRAY);
-
-  ROS_INFO("Median blurring...");
   img_blur = Mat(this->img_bin.size(), this->img_bin.type());
   medianBlur(this->img_bin, img_blur, median_blur_size);
-
-  ROS_INFO("Running thresholding operation...");
   img_thresh = Mat(this->img_bin.size(), this->img_bin.type());
   threshold(img_blur, img_thresh, bin_threshold, 255, THRESH_BINARY_INV);
-
-  ROS_INFO("Running post-thresholding blurring...");
   blur(img_thresh, this->img_bin, Size(blur_kernel_size, blur_kernel_size));
 }
 
@@ -101,16 +188,12 @@ vector< vector<Point> > PieceParser::findPieces(
   int i;
   vector< vector<Point> > piece_contours;
   vector< vector<Point> > piece_candidates;
-  ROS_INFO("Finding puzzle piece contours...");
   findContours(this->img_bin, piece_candidates, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE);
-
-  ROS_INFO("Checking puzzle piece candidates...");
 
   for(i = 0; i < piece_candidates.size(); ++i)
   {
     if(this->checkPiece(piece_candidates[i], area_threshold, num_pixel_threshold, edge_distance_threshold))
     {
-      ROS_INFO("Adding piece to valid piece list...");
       piece_contours.push_back(piece_candidates[i]);
     }
     else
@@ -132,19 +215,15 @@ bool PieceParser::checkPiece(
   vector<Point>::iterator pixel_iter = piece_candidate.begin();
   int num_pixels_near_edges = 0;
 
-  ROS_INFO("Checking area of current piece candidate...");
   // Ensure that the area is above a certain threshold.
   if(contourArea(piece_candidate) < area_threshold)
   {
-    ROS_INFO("Piece candidate invalidated due to insufficient area.");
     piece_validity = false;
   }
   else
   {
     // No operation
   }
-
-  ROS_INFO("Checking distance of piece candidate from edge...");
 
   // Ensure that the piece candidate is not close to the edge of the image.
   while((piece_validity == true) && (pixel_iter != piece_candidate.end()))
@@ -158,7 +237,6 @@ bool PieceParser::checkPiece(
 
       if(num_pixels_near_edges >= num_pixel_threshold)
       {
-        ROS_INFO("Piece candidate invalidated due to proximity to edge.");
         piece_validity = false;
 
         break;
@@ -176,102 +254,104 @@ bool PieceParser::checkPiece(
     ++pixel_iter;
   }
 
-  return true;
+  return piece_validity;
 }
 
 /*
-PieceParser::PieceParser(
-    ros::NodeHandle *nh,
-    string img_file_name,
-    bool use_roi,
-    int x,
-    int y,
-    int width,
-    int height)
-*/
+   PieceParser::PieceParser(
+   ros::NodeHandle *nh,
+   string img_file_name,
+   bool use_roi,
+   int x,
+   int y,
+   int width,
+   int height)
+   */
 /*
-{
-  this->nh = nh;
-  Mat img_raw = imread(img_file_name, CV_LOAD_IMAGE_COLOR);
+   {
+   this->nh = nh;
+   Mat img_raw = imread(img_file_name, CV_LOAD_IMAGE_COLOR);
 
-  if(img_raw.data)
-  {
-    if(use_roi)
-    {
-      this->region_of_interest = Rect(x, y, width, height);
-      this->img_bin = Mat(Size(region_of_interest.width, region_of_interest.height), img_raw.type());
-      cvtColor(img_raw(region_of_interest), this->img_bin, CV_RGB2GRAY);
-    }
-    else
-    {
-      this->img_bin = Mat(img_raw.size(), img_raw.type());
-      cvtColor(img_raw, this->img_bin, CV_RGB2GRAY);
-    }
-  }
-  else
-  {
-    throw runtime_error("Could not open file.");
-  }
+   if(img_raw.data)
+   {
+   if(use_roi)
+   {
+   this->region_of_interest = Rect(x, y, width, height);
+   this->img_bin = Mat(Size(region_of_interest.width, region_of_interest.height), img_raw.type());
+   cvtColor(img_raw(region_of_interest), this->img_bin, CV_RGB2GRAY);
+   }
+   else
+   {
+   this->img_bin = Mat(img_raw.size(), img_raw.type());
+   cvtColor(img_raw, this->img_bin, CV_RGB2GRAY);
+   }
+   }
+   else
+   {
+   throw runtime_error("Could not open file.");
+   }
 
-  ROS_INFO_STREAM("Median blurring image of type " << img_bin.type() << "...");
-  Mat img_blur(this->img_bin.size(), this->img_bin.type());
-  medianBlur(this->img_bin, img_blur, 5);
+   ROS_INFO_STREAM("Median blurring image of type " << img_bin.type() << "...");
+   Mat img_blur(this->img_bin.size(), this->img_bin.type());
+   medianBlur(this->img_bin, img_blur, 5);
 
-  ROS_INFO_STREAM("Thresholding image...");
-  Mat img_thresh(img_blur.size(), img_blur.type());
-  threshold(img_blur, img_thresh, 120, 255, THRESH_BINARY_INV);
+   ROS_INFO_STREAM("Thresholding image...");
+   Mat img_thresh(img_blur.size(), img_blur.type());
+   threshold(img_blur, img_thresh, 120, 255, THRESH_BINARY_INV);
 
-  ROS_INFO_STREAM("Blurring image...");
-  blur(img_thresh, img_blur, Size(3, 3));
+   ROS_INFO_STREAM("Blurring image...");
+   blur(img_thresh, img_blur, Size(3, 3));
 
-  ROS_INFO_STREAM("Coloring connected components...");
-  vector<Vec3b> colors(num_labels + 1);
-  colors[0] = Vec3b(0, 0, 0);
-  int i;
-  int j;
+   ROS_INFO_STREAM("Coloring connected components...");
+   vector<Vec3b> colors(num_labels + 1);
+   colors[0] = Vec3b(0, 0, 0);
+   int i;
+   int j;
 
-  for(i = 1; i <= num_labels; ++i)
-  {
-    colors[i] = Vec3b(rand() % 256, rand() % 256, rand() % 256);
-  }
+   for(i = 1; i <= num_labels; ++i)
+   {
+   colors[i] = Vec3b(rand() % 256, rand() % 256, rand() % 256);
+   }
 
-  Mat img_labelled(img_blur.size(), CV_8UC3);
+   Mat img_labelled(img_blur.size(), CV_8UC3);
 
-  for(i = 0; i < img_labelled.rows; ++i)
-  {
-    for(j = 0; j < img_labelled.cols; ++j)
-    {
-      img_labelled.at<Vec3b>(i, j) = colors[img_labels.at<int>(i, j)];
-    }
-  }
+   for(i = 0; i < img_labelled.rows; ++i)
+   {
+   for(j = 0; j < img_labelled.cols; ++j)
+   {
+   img_labelled.at<Vec3b>(i, j) = colors[img_labels.at<int>(i, j)];
+   }
+   }
 
-  ROS_INFO_STREAM("Finding contours...");
-  vector< vector<Point> > contours;
-  findContours(img_blur, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE);
-  ROS_INFO_STREAM(contours.size() << " contours found.");
-  Mat img_edges = Mat::zeros(img_blur.size(), CV_8UC3);
-  vector<Point2f> centroids(contours.size());
+   ROS_INFO_STREAM("Finding contours...");
+   vector< vector<Point> > contours;
+   findContours(img_blur, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE);
+   ROS_INFO_STREAM(contours.size() << " contours found.");
+   Mat img_edges = Mat::zeros(img_blur.size(), CV_8UC3);
+   vector<Point2f> centroids(contours.size());
 
-  for(i = 0; i < contours.size(); ++i)
-  {
-    Moments mu = moments(contours[i], false);
-    centroids[i] = Point2f(mu.m10 / mu.m00, mu.m01 / mu.m00);
-    Scalar colour = Scalar(rand() % 256, rand() % 256, rand() % 256);
-    drawContours(img_edges, contours, i, colour, 2); // Thickness = 2
-    circle(img_edges, centroids[i], 16, colour, -1, 8, 0);
-  }
+   for(i = 0; i < contours.size(); ++i)
+   {
+   Moments mu = moments(contours[i], false);
+   centroids[i] = Point2f(mu.m10 / mu.m00, mu.m01 / mu.m00);
+   Scalar colour = Scalar(rand() % 256, rand() % 256, rand() % 256);
+   drawContours(img_edges, contours, i, colour, 2); // Thickness = 2
+   circle(img_edges, centroids[i], 16, colour, -1, 8, 0);
+   }
 
-  string out_file_name;
-  // imwrite(out_file_name, img_edges);
+string out_file_name;
+// imwrite(out_file_name, img_edges);
 
-  namedWindow("display_window", WINDOW_AUTOSIZE);
-  imshow("display_window", img_edges);
-  waitKey(0);
+namedWindow("display_window", WINDOW_AUTOSIZE);
+imshow("display_window", img_edges);
+waitKey(0);
 }
 */
 
 int main(int argc, char **argv)
 {
+  colours.push_back(Scalar(int(0.741 * 256), int(0.447 * 256), int(0.000 * 256)));
+  colours.push_back(Scalar(int(0.098 * 256), int(0.325 * 256), int(0.850 * 256)));
   ros::init(argc, argv, "piece_parser");
   ros::NodeHandle nh;
   PieceParser p(nh);
