55baa627686a098e57b6dd373a987ee714807428
Mardava Gubbi <mgubbi1@jhu.edu>
2019-04-29 12:22:31 -0400

    Add area threshold to dynamic reconfigure

--- src/piece_parser.cpp
+++ src/piece_parser.cpp
@@ -57,6 +57,9 @@ class PieceParser
      */
     image_transport::Publisher vis_pub;
     int bin_threshold;
+    int area_threshold;
+    int cos_point_skip;
+    int num_corners;
     dynamic_reconfigure::Server<jps_puzzle_piece::PieceParserConfig> reconf_server;
     dynamic_reconfigure::Server<jps_puzzle_piece::PieceParserConfig>::CallbackType reconf_callback_type;
 
@@ -99,7 +102,6 @@ class PieceParser
      */
     bool checkPiece(
         vector<Point> piece_candidate,
-        int area_threshold,
         int num_pixel_threshold,
         int edge_distance_threshold);
     /*
@@ -113,6 +115,13 @@ class PieceParser
     void dynamicReconfigureCallback(
         jps_puzzle_piece::PieceParserConfig& config,
         uint32_t level);
+    /*
+     * \brief Get the most likely corner vertices of the given piece
+     *
+     * \param[in] piece_contour A vector of points forming the piece contour
+     * \param[in] num_corners The number of corners to be returned
+     */
+    vector<int> getCorners(vector<Point> piece_contour);
 
   public:
     /*
@@ -123,6 +132,23 @@ class PieceParser
      * \author Mardava Gubbi <mgubbi1@jhu.edu>
      */
     PieceParser(ros::NodeHandle& nh);
+    /*
+     * \brief Find all puzzle pieces in the given image
+     *
+     * \param[in] area_threshold The minimum area for a puzzle piece
+     * \param[in] num_pixel_threshold The maximum number of pixels close to the
+     * image edge
+     * \param[in] edge_distance_threshold The distance from the edge outside of
+     * which the piece must be
+     *
+     * \return The vector of contours of valid puzzle pieces in the supplied
+     * image
+     *
+     * \author Mardava Gubbi <mgubbi1@jhu.edu>
+     */
+    vector< vector< Point> > findPieces(
+        int num_pixel_threshold,
+        int edge_distance_threshold);
     /*
      * \brief Get the edges of the puzzle piece
      *
@@ -142,24 +168,6 @@ class PieceParser
         int aperture_size,
         double harris_free_param);
     /*
-     * \brief Find all puzzle pieces in the given image
-     *
-     * \param[in] area_threshold The minimum area for a puzzle piece
-     * \param[in] num_pixel_threshold The maximum number of pixels close to the
-     * image edge
-     * \param[in] edge_distance_threshold The distance from the edge outside of
-     * which the piece must be
-     *
-     * \return The vector of contours of valid puzzle pieces in the supplied
-     * image
-     *
-     * \author Mardava Gubbi <mgubbi1@jhu.edu>
-     */
-    vector< vector< Point> > findPieces(
-        int area_threshold,
-        int num_pixel_threshold,
-        int edge_distance_threshold);
-    /*
      * \brief Callback function for the image subscriber object
      *
      * \param[in] msg The message containing the latest image from the camera
@@ -169,19 +177,72 @@ class PieceParser
     void imageSubscriberCallback(const sensor_msgs::ImageConstPtr& msg);
 };
 
+PieceParser::PieceParser(ros::NodeHandle& nh)
+{
+  string input_image_topic;
+  string output_image_topic;
+  ROS_INFO("Initializing piece parser...");
+  this->nh = ros::NodeHandle(nh);
+  this->img_transport = new image_transport::ImageTransport(this->nh);
+  this->bin_threshold = 105;
+  this->area_threshold = 100000;
+  this->num_corners = 4;
+
+  this->image_pub = this->nh.advertise<jps_puzzle_piece::ImageWithContour>(
+      "output_image",
+      1000);
+  this->vis_pub = this->img_transport->advertise("vis_image", 1000);
+
+  this->image_sub = this->img_transport->subscribe(
+      "input_image",
+      1,
+      &PieceParser::imageSubscriberCallback,
+      this);
+  this->reconf_callback_type = boost::bind(&PieceParser::dynamicReconfigureCallback, this, _1, _2);
+  this->reconf_server.setCallback(this->reconf_callback_type);
+}
+
+void PieceParser::binarizeImage(
+    Mat img_input,
+    int median_blur_size,
+    int bin_threshold,
+    int blur_kernel_size)
+{
+  Mat img_blur;
+  Mat img_thresh;
+
+  if(this->img_bin.rows == 0)
+  {
+    this->img_gray = Mat(img_input.size(), CV_8UC1);
+    this->img_bin = Mat(this->img_gray.size(), this->img_gray.type());
+  }
+  else
+  {
+    // No operation
+  }
+
+  cvtColor(img_input, this->img_gray, CV_RGB2GRAY);
+  img_blur = Mat(this->img_gray.size(), this->img_gray.type());
+  medianBlur(this->img_gray, img_blur, median_blur_size);
+  img_thresh = Mat(this->img_gray.size(), this->img_gray.type());
+  threshold(img_blur, img_thresh, bin_threshold, 255, THRESH_BINARY_INV);
+  blur(img_thresh, this->img_bin, Size(blur_kernel_size, blur_kernel_size));
+}
+
 bool PieceParser::checkPiece(
     vector<Point> piece_candidate,
-    int area_threshold,
     int num_pixel_threshold,
     int edge_distance_threshold)
 {
   bool piece_validity = true;
   vector<Point>::iterator pixel_iter = piece_candidate.begin();
   int num_pixels_near_edges = 0;
+  int contour_area = contourArea(piece_candidate);
 
   // Ensure that the area is above a certain threshold.
-  if(contourArea(piece_candidate) < area_threshold)
+  if(contour_area < this->area_threshold)
   {
+    ROS_INFO_STREAM("Contour area error: " << contour_area << " < area_threshold[=" << this->area_threshold << "]");
     piece_validity = false;
   }
   else
@@ -201,6 +262,7 @@ bool PieceParser::checkPiece(
 
       if(num_pixels_near_edges >= num_pixel_threshold)
       {
+        ROS_INFO_STREAM("Piece near edge error: Too many pixels near edge");
         piece_validity = false;
 
         break;
@@ -221,27 +283,144 @@ bool PieceParser::checkPiece(
   return piece_validity;
 }
 
-PieceParser::PieceParser(ros::NodeHandle& nh)
+void PieceParser::dynamicReconfigureCallback(
+    jps_puzzle_piece::PieceParserConfig& config,
+    uint32_t level)
 {
-  string input_image_topic;
-  string output_image_topic;
-  ROS_INFO("Initializing piece parser...");
-  this->nh = ros::NodeHandle(nh);
-  this->img_transport = new image_transport::ImageTransport(this->nh);
-  this->bin_threshold = 105;
+  this->bin_threshold = config.bin_threshold;
+  this->area_threshold = config.area_threshold;
+  this->cos_point_skip = config.cos_point_skip;
+}
 
-  this->image_pub = this->nh.advertise<jps_puzzle_piece::ImageWithContour>(
-      "output_image",
-      1000);
-  this->vis_pub = this->img_transport->advertise("vis_image", 1000);
+vector< vector<Point> > PieceParser::findPieces(
+    int num_pixel_threshold,
+    int edge_distance_threshold)
+{
+  int i;
+  vector< vector<Point> > piece_contours;
+  vector< vector<Point> > piece_candidates;
+  findContours(
+      this->img_bin,
+      piece_candidates,
+      CV_RETR_EXTERNAL,
+      CV_CHAIN_APPROX_NONE);
+  ROS_INFO_STREAM(piece_candidates.size() << " piece candidates found.");
 
-  this->image_sub = this->img_transport->subscribe(
-      "input_image",
-      1,
-      &PieceParser::imageSubscriberCallback,
-      this);
-  this->reconf_callback_type = boost::bind(&PieceParser::dynamicReconfigureCallback, this, _1, _2);
-  this->reconf_server.setCallback(this->reconf_callback_type);
+  for(i = 0; i < piece_candidates.size(); ++i)
+  {
+    if(
+        this->checkPiece(
+          piece_candidates[i],
+          num_pixel_threshold,
+          edge_distance_threshold))
+    {
+      piece_contours.push_back(piece_candidates[i]);
+    }
+    else
+    {
+      // No operation
+    }
+  }
+
+  return piece_contours;
+}
+
+vector<int> PieceParser::getCorners(vector<Point> piece_contour)
+{
+  double x_diff;
+  double y_diff;
+  double a_square;
+  double cos_theta;
+  // Distance between i and i + this->cos_point_skip
+  double dist[piece_contour.size()];
+  double dist_square[piece_contour.size()];
+  // Minimum cos indices and values.
+  double min_cos_theta[this->num_corners];
+  vector<int> corner_indices(this->num_corners);
+  int i_prev;
+  int i_next;
+  int i;
+  int j;
+  int k;
+  int num_min_points_found = 0;
+
+  for(i = 0; i < this->num_corners; ++i)
+  {
+    min_cos_theta[i] = 2.0;
+    corner_indices[i] = -1;
+  }
+
+  for(i = 0; i < piece_contour.size(); ++i)
+  {
+    // Form a triangle ABC with vertices i, i_next, and i_prev respectively.
+    i_prev =
+      (i + piece_contour.size() - this->cos_point_skip)
+      % piece_contour.size();
+    i_next = (i + this->cos_point_skip) % piece_contour.size();
+
+    // Compute the side A and its square.
+    x_diff = piece_contour[i_next].x - piece_contour[i_prev].x;
+    y_diff = piece_contour[i_next].y - piece_contour[i_prev].y;
+    a_square = ((x_diff * x_diff) + (y_diff - y_diff));
+
+    if(i_prev > i)
+    {
+      // Compute the size B and its square.
+      x_diff = piece_contour[i].x - piece_contour[i_prev].x;
+      y_diff = piece_contour[i].y - piece_contour[i_prev].y;
+      dist_square[i_prev] = (x_diff * x_diff) + (y_diff - y_diff);
+      dist[i_prev] = sqrt(dist_square[i_prev]);
+    }
+    else
+    {
+      // No operation
+    }
+
+    if(i < i_next)
+    {
+      // Compute the side C and its square.
+      x_diff = piece_contour[i_next].x - piece_contour[i].x;
+      y_diff = piece_contour[i_next].y - piece_contour[i].y;
+      dist_square[i] = sqrt((x_diff * x_diff) + (y_diff - y_diff));
+      dist[i] = sqrt(dist_square[i]);
+    }
+    else
+    {
+      // No operation
+    }
+
+    // Compute cos theta and compare with current minimum.
+    cos_theta =
+      abs((dist_square[i_prev] + dist_square[i_next] - a_square)
+          / (2 * dist[i_prev] * dist[i_next]));
+
+    for(j = 0; j < this->num_corners; ++j)
+    {
+      if(cos_theta >= min_cos_theta[j])
+      {
+        // Insert both the cos value and the index into the list and break.
+        for(k = 0; k < j; ++k)
+        {
+          min_cos_theta[k] = min_cos_theta[k + 1];
+          corner_indices[k] = corner_indices[k + 1];
+        }
+
+        min_cos_theta[j] = cos_theta;
+        corner_indices[j] = i;
+        ++num_min_points_found;
+
+        break;
+      }
+      else
+      {
+        // No operation
+      }
+    }
+  }
+
+  ROS_INFO_STREAM(num_min_points_found << " corner points found and pruned to " << this->num_corners << ".");
+
+  return corner_indices;
 }
 
 vector<Point> PieceParser::getEdges(
@@ -325,60 +504,25 @@ vector<Point> PieceParser::getEdges(
   return corner_points_vec;
 }
 
-vector< vector<Point> > PieceParser::findPieces(
-    int area_threshold,
-    int num_pixel_threshold,
-    int edge_distance_threshold)
-{
-  int i;
-  vector< vector<Point> > piece_contours;
-  vector< vector<Point> > piece_candidates;
-  findContours(
-      this->img_bin,
-      piece_candidates,
-      CV_RETR_EXTERNAL,
-      CV_CHAIN_APPROX_NONE);
-
-  for(i = 0; i < piece_candidates.size(); ++i)
-  {
-    if(
-        this->checkPiece(
-          piece_candidates[i],
-          area_threshold,
-          num_pixel_threshold,
-          edge_distance_threshold))
-    {
-      piece_contours.push_back(piece_candidates[i]);
-    }
-    else
-    {
-      // No operation
-    }
-  }
-
-  return piece_contours;
-}
-
-void PieceParser::dynamicReconfigureCallback(
-    jps_puzzle_piece::PieceParserConfig& config,
-    uint32_t level)
-{
-  this->bin_threshold = config.bin_threshold;
-}
 void PieceParser::imageSubscriberCallback(const sensor_msgs::ImageConstPtr& msg)
 {
+  double corner_angles[6];
+  double dist_center;
+  double min_dist_center = 1e12;
+  double resize_factor = 0.8;
   int central_index = -1;
   int i;
+  int i_prev;
+  int i_next;
   int j;
-  double resize_factor = 0.4;
-  double min_dist_center = 1e12;
-  double dist_center;
+  int k;
+  jps_puzzle_piece::ImageWithContour image_msg;
   Mat img_raw;
   Mat img_vis;
-  vector< vector<Point> > piece_contours;
-  vector<Point> harris_corners;
-  jps_puzzle_piece::ImageWithContour image_msg;
   sensor_msgs::ImagePtr vis_msg;
+  vector<Point> harris_corners;
+  vector< vector<Point> > piece_contours;
+  vector<int> corner_indices;
 
   // Binarize the image with preset thresholds.
   // TODO: tweak the thresholds if need be.
@@ -394,18 +538,18 @@ void PieceParser::imageSubscriberCallback(const sensor_msgs::ImageConstPtr& msg)
 
   // Obtain the connected components and their centroids.
   ROS_INFO("Finding puzzle pieces in image...");
-  piece_contours = this->findPieces(100000, 100, 20);
+  piece_contours = this->findPieces(100, 20);
   ROS_INFO_STREAM(piece_contours.size() << " piece contours found.");
+  // Ease visualization and debugging with a binarized image.
   cvtColor(this->img_bin, img_vis, CV_GRAY2BGR);
 
+  // Draw all valid piece contours.
   for(i = 0; i < piece_contours.size(); ++i)
   {
     drawContours(img_vis, piece_contours, i, colours[0], 2);
   }
 
-  vis_msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", img_vis).toImageMsg();
-  this->vis_pub.publish(vis_msg);
-
+  // Determine the centroids of the contours.
   ROS_INFO("Finding centroids of pieces in image...");
   vector<Point2f> centroids(piece_contours.size());
   Point img_center(img_raw.cols / 2, img_raw.rows / 2);
@@ -414,6 +558,7 @@ void PieceParser::imageSubscriberCallback(const sensor_msgs::ImageConstPtr& msg)
   {
     Moments mu = moments(piece_contours[i], false);
     centroids[i] = Point2f(mu.m10 / mu.m00, mu.m01 / mu.m00);
+    // Isolate the contour closest to the center of the image.
     dist_center =
       sqrt(
           ((centroids[i].x - img_center.x) * (centroids[i].x - img_center.x))
@@ -430,12 +575,51 @@ void PieceParser::imageSubscriberCallback(const sensor_msgs::ImageConstPtr& msg)
     }
   }
 
-  // TODO: tweak edge classification parameters if need be.
   if(central_index >= 0)
   {
-    ROS_INFO("Finding Harris corners...");
-    harris_corners = getEdges(piece_contours[central_index], 5, 5, 0.04);
+    // Visualize the centroid of the central piece.
+    ROS_INFO("Plotting centroid of central piece...");
+    circle(
+        img_vis,
+        centroids[central_index],
+        16,
+        colours[0],
+        -1,
+        8,
+        0);
+    //    ROS_INFO("Finding Harris corners...");
+    //    harris_corners = getEdges(piece_contours[central_index], 5, 5, 0.04);
+    //
+    //    for(i = 0; i < harris_corners.size(); ++i)
+    //    {
+    //      circle(
+    //          img_vis,
+    //          Point2f(harris_corners[i].x, harris_corners[i].y),
+    //          16,
+    //          colours[1],
+    //          -1,
+    //          8,
+    //          0);
+    //    }
 
+    ROS_INFO_STREAM("Fetching " << this->num_corners << " corner pieces...");
+    corner_indices = this->getCorners(piece_contours[central_index]);
+
+    for(i = 0; i < corner_indices.size(); ++i)
+    {
+      circle(
+          img_vis,
+          Point2f(
+            piece_contours[central_index][corner_indices[i]].x,
+            piece_contours[central_index][corner_indices[i]].y),
+          16,
+          colours[1],
+          -1,
+          8,
+          0);
+    }
+
+    // Publish the images and visualization.
     image_msg.header.stamp = ros::Time::now();
     cv_bridge::CvImage(image_msg.header, "bgr8", img_raw).toImageMsg(
         image_msg.image);
@@ -451,39 +635,19 @@ void PieceParser::imageSubscriberCallback(const sensor_msgs::ImageConstPtr& msg)
       image_msg.contour_px.push_back(pt_temp);
     }
 
+    ROS_INFO_STREAM(
+        "Publishing "
+        << piece_contours[central_index].size()
+        << " points on central contour...");
     this->image_pub.publish(image_msg);
   }
   else
   {
     ROS_INFO("No piece found.");
   }
-}
 
-void PieceParser::binarizeImage(
-    Mat img_input,
-    int median_blur_size,
-    int bin_threshold,
-    int blur_kernel_size)
-{
-  Mat img_blur;
-  Mat img_thresh;
-
-  if(this->img_bin.rows == 0)
-  {
-    this->img_gray = Mat(img_input.size(), CV_8UC1);
-    this->img_bin = Mat(this->img_gray.size(), this->img_gray.type());
-  }
-  else
-  {
-    // No operation
-  }
-
-  cvtColor(img_input, this->img_gray, CV_RGB2GRAY);
-  img_blur = Mat(this->img_gray.size(), this->img_gray.type());
-  medianBlur(this->img_gray, img_blur, median_blur_size);
-  img_thresh = Mat(this->img_gray.size(), this->img_gray.type());
-  threshold(img_blur, img_thresh, bin_threshold, 255, THRESH_BINARY_INV);
-  blur(img_thresh, this->img_bin, Size(blur_kernel_size, blur_kernel_size));
+  vis_msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", img_vis).toImageMsg();
+  this->vis_pub.publish(vis_msg);
 }
 
 int main(int argc, char **argv)
